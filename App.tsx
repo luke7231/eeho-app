f4f06451ab6c2648850a7e3d70eb19f19f8ab760
luke <luke@lukes-MacBook-Air.local>
2023-11-16 21:57:28 -0800

    refactor: safe-area 컬러코드 상수로 추출

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { SafeAreaView, View } from "react-native";
 import Navigation from "./Navigation";
 
+const NOTCH_COLOR = "#FAFFF1";
+
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#FAFFF1" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: NOTCH_COLOR }}>
       <Navigation />
     </SafeAreaView>
   );
