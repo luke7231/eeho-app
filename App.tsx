7b7206cb2a352b1eeb205ab0723bba4bbbb838eb
luke <luke@lukes-MacBook-Air.local>
2023-11-20 11:28:14 -0800

    feat: safe-area 조정 및 안드로이드 status bar 설정 추가 및 안드로이드 paddingBottom 추가

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, StatusBar, View } from "react-native";
 import Navigation from "./Navigation";
-
+import { useEffect } from "react";
+import styles from "./styles";
 const NOTCH_COLOR = "#FAFFF1";
 
 export default function App() {
+  useEffect(() => {
+    // notificationListener.current =
+    //   Notifications.addNotificationReceivedListener((notification) => {
+    //     setNotification(notification);
+    //   });
+    // responseListener.current =
+    //   Notifications.addNotificationResponseReceivedListener((response) => {
+    //     console.log(response.notification.request.content);
+    //     /*
+    //       * 메시지 클릭 시 카메라 앱으로 보낸다.
+    //       * 근데  데이터를 담아서 보내야한다.
+    //       * 그럴려면 푸시에 데이터가 담겨있어야한다.
+    //       * 담긴다. data = {who: "daddy"}로 잘 왔다.
+    //     ​ */
+    //     navigation.navigate("Camera");
+    //   });
+  }, []);
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: NOTCH_COLOR }}>
-      <Navigation />
-    </SafeAreaView>
+    <>
+      <StatusBar backgroundColor={NOTCH_COLOR} barStyle={"dark-content"} />
+      <SafeAreaView style={styles.safeAreaView}>
+        <Navigation />
+      </SafeAreaView>
+    </>
   );
 }
