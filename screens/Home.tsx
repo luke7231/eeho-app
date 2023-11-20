9eb62b91d5cac17e28fa640b1669e4791b7db7ba
luke <luke@lukes-MacBook-Air.local>
2023-11-20 11:46:18 -0800

    feat: permission(noti, camera) 권한요청 로직 추가 & 안드로이드 백버튼 핸들러 추가

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,50 +1,50 @@
-import { View } from "react-native";
+import { BackHandler, Button, Text, View } from "react-native";
 import WebView, { WebViewMessageEvent } from "react-native-webview";
 import { Camera } from "expo-camera";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../types";
-
+import { registerForPushNotificationsAsync } from "../utils/push-notification";
+import { useEffect, useRef } from "react";
+import * as Notifications from "expo-notifications";
+import { useAndroidBackEffect } from "../hooks/useAndroidBackEffect";
 export type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
 
 export default function Home({ navigation }: HomeScreenProps) {
   const [permission, requestPermission] = Camera.useCameraPermissions();
-
+  const webViewRef = useRef<WebView>(null);
   if (!permission?.granted) {
-    // Camera permissions are not granted yet
     requestPermission();
   }
   async function onMessage(e: WebViewMessageEvent) {
     const data = e.nativeEvent.data; // 로그인 하기
-    if (data === "camera") {
-      navigation.navigate("Camera");
+    const parsedData = JSON.parse(data);
+    if (parsedData.type === "camera_open") {
+      navigation.navigate("Camera", parsedData.payload);
     }
   }
+  const sendPostMessage = (token: string) => {
+    webViewRef.current?.postMessage(token);
+    console.log(token, "완료");
+  };
+
+  useAndroidBackEffect(webViewRef);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      token ? sendPostMessage(token) : null
+    );
+
+    Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response.notification.request.content);
+    });
+  }, [webViewRef.current]);
   return (
     <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: "http://192.168.0.69:3000" }}
+        ref={webViewRef}
+        source={{ uri: "http://172.16.225.128:3000" }}
         onMessage={onMessage}
       />
-      {/* <Text>Your expo push token: {expoPushToken}</Text>
-          <View style={{ alignItems: "center", justifyContent: "center" }}>
-            <Text>
-              Title: {notification && notification.request.content.title}{" "}
-            </Text>
-            <Text>
-              Body: {notification && notification.request.content.body}
-            </Text>
-            <Text>
-              Data:{" "}
-              {notification &&
-                JSON.stringify(notification.request.content.data)}
-            </Text>
-          </View> */}
-      {/* <Button
-        title="Press to Send Notification"
-        onPress={async () => {
-          await sendPushNotification(expoPushToken);
-        }}
-      /> */}
     </View>
   );
 }
