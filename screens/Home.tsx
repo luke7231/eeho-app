c4fe3b70c70c2a19cd49092f01e4143f7b0b27a3
luke <luke@lukes-MacBook-Air.local>
2023-11-21 16:08:34 -0800

    feat: home async storage 통해서 토큰 받고 카메라 이동

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -15,6 +15,7 @@ import { useEffect, useRef } from "react";
 import * as Notifications from "expo-notifications";
 import { useAndroidBackEffect } from "../hooks/useAndroidBackEffect";
 import styles, { NOTCH_COLOR } from "../styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
 
 export default function Home({ navigation }: HomeScreenProps) {
@@ -28,6 +29,9 @@ export default function Home({ navigation }: HomeScreenProps) {
     const parsedData = JSON.parse(data);
     if (parsedData.type === "camera_open") {
       navigation.navigate("Camera", parsedData.payload);
+    } else if (parsedData.type === "store_token") {
+      const stringValue = JSON.stringify(parsedData.payload.token);
+      await AsyncStorage.setItem("jwt", stringValue);
     }
   }
   const sendPostMessage = (token: string) => {
@@ -42,8 +46,23 @@ export default function Home({ navigation }: HomeScreenProps) {
       token ? sendPostMessage(token) : null
     );
 
+    async function naviToCamera(userIds: string[]) {
+      const token = await AsyncStorage.getItem("jwt");
+      console.log(token, "async");
+      if (!token) return;
+      const tokenWithoutQuotes = token.replace(/^"(.*)"$/, "$1");
+
+      navigation.navigate("Camera", {
+        token: tokenWithoutQuotes,
+        userIds,
+      });
+    }
+
     Notifications.addNotificationResponseReceivedListener((response) => {
       console.log(response.notification.request.content);
+      // from: userId
+      // userId로 보낸다.
+      naviToCamera([response.notification.request.content.data.from]);
     });
   }, [webViewRef.current]);
   return (
